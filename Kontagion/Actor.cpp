7e6ac2f51c1152db863ec8af52462754525f9f5a
Samuel Shen <sam.shen321@gmail.com>
2020-02-14 16:51:22 -0800

    part1 templates

--- Kontagion/Actor.cpp
+++ Kontagion/Actor.cpp
@@ -4,15 +4,69 @@
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
 
-
-Actor::Actor(int imageID, int startX, int startY, Direction startDirection, int depth)
+//ACTOR CLASS
+Actor::Actor(int imageID, int startX, int startY, Direction startDirection, int depth, int hp)
 :GraphObject(imageID, startX, startY, startDirection, depth)
+{
+    m_HP = hp;
+    m_isAlive = true;
+}
+
+Actor::~Actor(){
+    ;
+}
+
+bool Actor::isAlive(){
+    return m_isAlive;
+}
+
+void Actor::setDead(){
+    m_isAlive = false;
+}
+
+int Actor::getHP(){
+    return m_HP;
+}
+
+void Actor::setHP(int hp){
+    m_HP = hp;
+}
+
+
+//DIRT
+//
+
+Dirt::Dirt(int startX, int startY)
+:Actor(IID_DIRT, startX, startY, 90, 1, 0)
 {
     
 }
 
-Dirt::Dirt(int imageID, int startX, int startY, Direction startDirection, int depth)
-:Actor(imageID, startX, startY, startDirection, depth)
+Dirt::~Dirt(){
+    ;
+}
+
+bool Dirt::doSomething(){
+    return true;
+}
+
+Socrates::Socrates()
+:Actor(IID_PLAYER, 0, 128, 0, 0, 100)
+{
+    m_sprayCount = 20;
+    m_flameCount = 5;
+}
+
+
+//FOOD
+//
+
+Food::Food(int startX, int startY)
+:Actor(IID_FOOD, startX, startY, 90, 1, 0)
 {
     
+}
+
+bool Food::doSomething(){
+    return true;
 }
--- Kontagion/Actor.h
+++ Kontagion/Actor.h
@@ -8,18 +8,45 @@
 class Actor: public GraphObject
 {
 public:
-    Actor(int imageID, int startX, int startY, Direction startDirection, int depth);
+    Actor(int imageID, int startX, int startY, Direction startDirection, int depth, int hp);
+    virtual ~Actor();
     virtual bool doSomething() = 0;
+    bool isAlive();
+    void setDead();
+    int getHP();
+    void setHP(int hp);
     
 private:
+    bool m_isAlive;
+    int m_HP;
 };
 
 class Dirt: public Actor
 {
 public:
-    Dirt(int imageID, int startX, int startY, Direction startDirection, int depth);
+    Dirt(int startX, int startY);
+    virtual ~Dirt();
+    virtual bool doSomething();
     
 private:
 };
 
+class Socrates: public Actor
+{
+public:
+    Socrates();
+    virtual ~Socrates();
+    virtual bool doSomething();
+private:
+    int m_sprayCount;
+    int m_flameCount;
+};
+
+class Food: public Actor
+{
+public:
+    Food(int startX, int startY);
+    virtual bool doSomething();
+private:
+};
 #endif // ACTOR_H_
